bff368b0b0eb54e8e00c64d998640d17cd462eca
Raphael Ickovics <raphael.ickovics@imper.ai>
2025-05-29 23:45:16 +0300

    switched to s3

--- README.md
+++ README.md
@@ -11,6 +11,25 @@ To install the Photo-Slider project, you need to have [PDM](https://pdm.fming.de
   ```bash
   curl -sSL https://pdm-project.org/install-pdm.py | python3 -
   ```
+- Docker and Docker compose:
+  ```bash
+  sudo yum install docker -y
+  sudo service docker start
+  sudo usermod -a -G docker ec2-user
+  # Now log out, log back in and test docker
+  docker ps
+
+  sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose version
+  ```
+
+- Filestash installation
+  ```bash
+  mkdir filestash && cd filestash
+  curl -O https://downloads.filestash.app/latest/docker-compose.yml
+  docker-compose up -d
+  ```
 
 ### Installation Steps
 
--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,22 @@
 import argparse
-import pathlib
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
 from loguru import logger
 
-from google_drive_api import GoogleDriveAPI
 from maintainer import Maintainer
 from models import RuntimeConfig
-
-google_api: GoogleDriveAPI | None = None
-LOCAL_PHOTOS_PATH = pathlib.Path('./photos')
-DRIVE_PHOTOS_FOLDER = 'Samples'
-CREDENTIALS_PATH = pathlib.Path('credentials.json')
-
 
 maintainer: Maintainer | None = None
 
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
-    if not LOCAL_PHOTOS_PATH.exists():
-        LOCAL_PHOTOS_PATH.mkdir(parents=True)
-    global google_api, maintainer
-    google_api = GoogleDriveAPI(CREDENTIALS_PATH)
-    maintainer = Maintainer(google_api, LOCAL_PHOTOS_PATH, DRIVE_PHOTOS_FOLDER)
+    global maintainer
+    maintainer = Maintainer()
     logger.info('Starting maintainer')
     maintainer.start()
-    logger.info('Maintainer started')
     yield
     maintainer.stop()
     logger.info('Maintainer stopped')
@@ -38,19 +25,13 @@ async def lifespan(_app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-class PhotoNotFoundError(Exception):
-    pass
-
-
 @app.get('/photos/next')
 async def get_next_photo_endpoint() -> FileResponse:
-    photo_path = maintainer.get_next_photo()
-    logger.info('Getting next photo', photo_id=photo_path)
-    if not photo_path:
+    photo_url = maintainer.get_next_photo()
+    logger.info('Getting next photo', photo_id=photo_url)
+    if not photo_url:
         raise HTTPException(status_code=404, detail='No photos available')
-    if not (photo_path).exists():
-        raise PhotoNotFoundError()
-    return FileResponse(photo_path)
+    return FileResponse(photo_url)
 
 
 @app.get('/configuration')
